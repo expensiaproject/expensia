909631aff504abf67f06df3f1482cc17982801b2
Winda Wulan Wengkau <windawulanwengkau@gmail.com>
2025-12-02 06:03:09 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -3,8 +3,6 @@ import NewExpense from './pages/NewExpense';
 import MyExpenses from './pages/MyExpenses';
 import EditExpense from './pages/EditExpense';
 import MyReports from './pages/MyReports';
-import NewReport from './pages/NewReport';
-import EditReport from './pages/EditReport';
 import Assistant from './pages/Assistant';
 import AdminDashboard from './pages/AdminDashboard';
 import AdminExpenses from './pages/AdminExpenses';
@@ -24,8 +22,6 @@ export const PAGES = {
     "MyExpenses": MyExpenses,
     "EditExpense": EditExpense,
     "MyReports": MyReports,
-    "NewReport": NewReport,
-    "EditReport": EditReport,
     "Assistant": Assistant,
     "AdminDashboard": AdminDashboard,
     "AdminExpenses": AdminExpenses,
